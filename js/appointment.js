58b987c4015bda84fc3a70e825b68edf9069012b
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-10-29 17:46:26 +0100

    29102024

--- js/appointment.js
+++ js/appointment.js
@@ -1,14 +1,27 @@
-() => {
+(() => {
   const appoint = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  appoint.openModalBtn.addEventListener("click", toggleModal);
-  appoint.closeModalBtn.addEventListener("click", toggleModal);
+  if (appoint.openModalBtn && appoint.closeModalBtn && appoint.modal) {
+    appoint.openModalBtn.addEventListener("click", toggleModal);
+    appoint.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    appoint.modal.classList.toggle("is-hidden");
+    document.addEventListener("keydown", (event) => {
+      if (
+        event.key === "Escape" &&
+        !appoint.modal.classList.contains("is-hidden")
+      ) {
+        toggleModal();
+      }
+    });
+
+    function toggleModal() {
+      appoint.modal.classList.toggle("is-hidden");
+    }
+  } else {
+    console.error("Some elements were not found");
   }
-};
+})();
